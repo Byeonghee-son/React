d7369b87a7b6b8399fd1df7089c97fff4bff0dd3
Byeonghee-son <0722ths@gmail.com>
2023-04-25 13:10:06 +0900

    MyClockTime

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 const MyClockTime = () =>{
-    let myTime = new Date().toLocaleTimeString();
+   let t = new Date().toLocaleTimeString();
+    const [myTime, setmyTime] = useState(t);
+    let cnt = 0;
+    setTimeout(setmyTime)(new Date().toLocaleTimeString(),1000)
     return(
         <footer>
+            
             <h1>{myTime}</h1>
         </footer>
 
